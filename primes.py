142407234a2bbab2ae90fd2f14e5ab29bd8c3f00
mrjilc <jilcbig@gmail.com>
2016-10-27 19:53:23 -0400

    py1

--- primes.py
+++ primes.py
@@ -1,16 +1,25 @@
 """
-Your module documentation here
+this module content one class call PrimeClass, one Function call is_prime, inside is_prime
+we are going to make all the operation, outside is the input asking the numer we want to know
+if is prime or not, and the print of that question
 """
-
 
 class PrimeClass(object):
     """
-    Your class documentation here
+   inside here is where we going to define the function for the operation
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        read the number and we proceed to do the operation num_int is the variable
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int < 2:     # if the number is less than 2 is not prime, so return false
+            return False
+        for i in range(2, num_int):  # create a For loop with a range between  2 and the number
+            if num_int % i == 0:    # if the modulo give a O is not prime, so return False
+                return False
+        return True
+
+num_prime = PrimeClass()  # create the object for the class
+num = int(input("Ingrese un numero: "))  # ask for the number
+print(num_prime.is_prime(num))  # show the result
