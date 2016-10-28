380af52fa9f1413ecb2e556a1bf4b8b9447cc50b
mrjilc <jilcbig@gmail.com>
2016-10-28 19:44:24 -0400

    Revert "py2"
    This reverts commit 5e3aa1e976ecd53f5fbc3b2ec3d438d156cb19bf.

--- calculator.py
+++ calculator.py
@@ -7,18 +7,27 @@ to do the operation, after that we going to print the result
 
 class CalculatorClass(object):
     """
-    Here we define two variables list and sum_num, sum_num we are going to perform the sum
+    Here we define two variables list and sum_num, the list we initiate in nothing only to fill
+     and in sum_num we are going to perform the sum
     """
-    list = [2, 3, 4, 5]
+    list = []
     sum_num = 0
+
+    def num_list(self):
+        x = int(input("Ingrese Tamaño De La Lista: "))  # ask for the quantity of numbers to sum
+        for i in range(x):
+            # fill the list
+            self.list.append(int(input("Ingrese Numero: ")))
 
     def sum(self):
         """
-        we are going to proceed to sum, whit a For loop and after the loop print the results
+        after fill the list we are going to proceed to sum, whit a For loop and after the loop
+        print the results
         """
         for i in self.list:
             self.sum_num += i
         print("La Suma De Los Datos Es: ", self.sum_num)
 
 result = CalculatorClass()  # create the object
+result.num_list()  # Call the fuction for the list
 result.sum()  # Do the sum and print the results
